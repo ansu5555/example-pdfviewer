b65c97e39655d0f336482099c50bae786f30fa63
ansuman5555 <ansumanghosh@infrrd.ai>
2020-04-03 02:03:49 +0530

    Added example for mobileview

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PDFViewer from 'pdf-viewer-reactjs'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <PDFViewer
+                document={{
+                    url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf',
+                }}
+                canvasCss="customCanvasCss"
+                scale={0.6}
+            />
+        </div>
+    )
 }
 
-export default App;
+export default App
