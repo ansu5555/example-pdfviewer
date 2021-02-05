b1cfab15fff37a5151c422b38d73cbb9c1da37b0
ansuman5555 <ansumanghosh@infrrd.ai>
2021-02-05 12:52:41 +0530

    new chenges for extrernal input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import PDFViewer from 'pdf-viewer-reactjs'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
     return (
-        <PDFViewer
-            document={{
-                url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf',
-            }}
-            canvasCss="customCanvasCss"
-            scale={0.6}
-        />
+        <div className="col-sm-12 text-center" id="url">
+            <h1 className="text-white bg-info rounded">
+                <a href="#url" className="text-reset text-decoration-none">
+                    Fetch PDF by URL
+                </a>
+            </h1>
+            <div className="border rounded">
+                <PDFViewer
+                    document={{
+                        url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf',
+                    }}
+                    navbarOnTop
+                    showThumbnail={{
+                        scale: 5,
+                        onTop: true,
+                        backgroundColor: '#000000',
+                        thumbCss: 'customThumbStyle',
+                        selectedThumbCss: 'customSelThumbStyle',
+                    }}
+                />
+            </div>
+        </div>
     )
 }
 
